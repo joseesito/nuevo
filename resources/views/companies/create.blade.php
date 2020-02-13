2ea8224658e750d2ea58a175eeb0bea129ffc6f0
ikkerxd <henrry.saa@gmail.com>
2020-02-13 09:50:46 -0500

    refatorizacion de company

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -6,7 +6,7 @@
 @stop
 
 @section('content')
-<div class="row">
+    <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
                 <h2>Agregar Empresa</h2>
@@ -17,54 +17,9 @@
         </div>
     </div>
 
-
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Sorry!</strong> Tienes problemas con tu input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
+    @include('partials.validation-errors')
 
     <form action="{{ route('companies.store') }}" method="POST">
-        @csrf
-        <div class="row">
-		    <div class="col-xs-12 col-sm-6">
-		        <div class="form-group">
-		            <label>Ruc:</label>
-		            <input type="text" name="ruc" class="form-control" placeholder="Ingrese el Nro de RUC">
-		        </div>
-		    </div>
-             <div class="col-xs-12 col-sm-6">
-                 <div class="form-group">
-                     <label>Nombre:</label>
-                     <input type="text" name="name" class="form-control" placeholder="Ingrese razón social">
-                 </div>
-             </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-12 col-sm-8">
-                <div class="form-group">
-                    <label>Dirección:</label>
-                    <input type="text" name="address" class="form-control" placeholder="Ingrese Dirección">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-4">
-                <div class="form-group">
-                    <label>telefono:</label>
-                    <input type="text" name="phone" class="form-control" placeholder="Ingrese Nro. de telefono">
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="text-center">
-                <button type="submit" class="btn btn-primary">Agregar</button>
-            </div>
-        </div>
-
+        @include('companies._form', ['btnText' => 'Guardar'])
     </form>
 @stop
--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Southern')
 
 @section('content_header')
-@stop
+@endsection
 
 @section('content')
     <div class="row">
@@ -17,55 +17,10 @@
         </div>
     </div>
 
-
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Sorry!</strong> Tienes problemas con tu input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
+    @include('partials.validation-errors')
 
     <form action="{{ route('companies.update', $company->id) }}" method="POST">
-        @csrf
         @method('PUT')
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <div class="form-group">
-                    <label>Ruc:</label>
-                    <input type="text" name="ruc" class="form-control" value="{{ $company->ruc }}" placeholder="Ingrese el Nro de RUC">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <div class="form-group">
-                    <label>Nombre:</label>
-                    <input type="text" name="name" class="form-control" value="{{ $company->name }}" placeholder="Ingrese razón social">
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-12 col-sm-8">
-                <div class="form-group">
-                    <label>Dirección:</label>
-                    <input type="text" name="address" class="form-control" value="{{ $company->address }}" placeholder="Ingrese Dirección">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-4">
-                <div class="form-group">
-                    <label>telefono:</label>
-                    <input type="text" name="phone" class="form-control" value="{{ $company->phone }}" placeholder="Ingrese Nro. de telefono">
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="text-center">
-                <button type="submit" class="btn btn-primary">Agregar</button>
-            </div>
-        </div>
-
+        @include('companies._form', ['btnText' => 'Actualizar'])
     </form>
-@stop
+@endsection
