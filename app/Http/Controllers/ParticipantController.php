1c38c025818c2bf425a4cde6355610369a4c1338
ikkerxd <henrry.saa@gmail.com>
2020-02-14 12:33:35 -0500

    creacion y actulializacion de participantes

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -21,15 +21,18 @@ class ParticipantController extends Controller
 
     public function index(Request $request)
     {
-        $data = User::join('model_has_roles','users.id','=','model_has_roles.model_id')
+        $data = User::select('users.id', 'users.document', 'users.name', 'users.last_name', 'users.position', 'users.area',
+            'users.state',
+            'companies.name as company', 'unities.name as unity',  'users.email')
+            ->join('model_has_roles','users.id','=','model_has_roles.model_id')
+            ->join('companies', 'companies.id', '=', 'users.company_id')
+            ->join('unities', 'unities.id', '=', 'users.unity_id')
             ->where('model_has_roles.role_id', 2)
-            ->orderBy('id','DESC')->paginate(5);
+            ->orderBy('id','DESC')->paginate(20);
 
         return view('participants.index',compact('data'))
             ->with('i', ($request->input('page', 1) - 1) * 5);
     }
-
-
 
     public function create()
     {
@@ -45,21 +48,22 @@ class ParticipantController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
+            'document' => 'required|min:8|alpha_num',
             'name' => 'required',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|same:confirm-password',
-            //'roles' => 'required'
+            'last_name' => 'required',
         ]);
 
         $input = $request->all();
-        $input['password'] = Hash::make($input['password']);
+        $input['email'] = $request->document.'p@mail.com';
+        // $input['password'] = Hash::make($input['password']);
+        $input['password'] = Hash::make($input['document']);
 
         $user = User::create($input);
 
         $user->assignRole('participante');
 
         return redirect()->route('participants.index')
-            ->with('Mensaje','El participante fue creado correctamente');
+            ->with('Mensaje','El participante '. $request->name .' ' .$request->last_name. ' fue registrado correctamente.');
     }
 
     public function show($id)
@@ -82,10 +86,9 @@ class ParticipantController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
+            'document' => 'required|min:8|alpha_num',
             'name' => 'required',
-            'email' => 'required|email|unique:users,email,'.$id,
-            'password' => 'same:confirm-password',
-            'roles' => 'required'
+            'last_name' => 'required',
         ]);
 
         $input = $request->all();
@@ -101,19 +104,19 @@ class ParticipantController extends Controller
         DB::table('model_has_roles')->where('model_id',$id)->delete();
 
 
-        $user->assignRole($request->input('roles'));
+        $user->assignRole('participante');
 
 
-        return redirect()->route('users.index')
-            ->with('Mensaje','User updated successfully');
+        return redirect()->route('participants.index')
+            ->with('Mensaje','El participante '. $request->name .' ' .$request->last_name. ' fue actualizado correctamente.');
     }
 
 
 
     public function destroy($id)
     {
-        User::find($id)->delete();
+        /*User::find($id)->delete();
         return redirect()->route('users.index')
-            ->with('Mensaje','User deleted successfully');
+            ->with('Mensaje','User deleted successfully');*/
     }
 }
--- resources/views/participants/create.blade.php
+++ resources/views/participants/create.blade.php
@@ -9,10 +9,10 @@
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Crear nuevo particpante</h2>
+            <h2>Crear nuevo participante</h2>
         </div>
         <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('users.index') }}"> Volver</a>
+            <a class="btn btn-primary" href="{{ route('participants.index') }}"> Volver</a>
         </div>
     </div>
 </div>
@@ -33,53 +33,58 @@
 
 {!! Form::open(array('route' => 'participants.store','method'=>'POST')) !!}
 <div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12">
+    <div class="col-xs-12 col-sm-3">
         <div class="form-group">
-            <strong>Nombre:</strong>
-            {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
+            <strong>DNI/Documento:</strong>
+            {!! Form::text('document', null, array('placeholder' => 'Ingrese el numero de documento','class' => 'form-control')) !!}
         </div>
-        </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
+    </div>
+    <div class="col-xs-12 col-sm-4">
         <div class="form-group">
-        <strong>Apellido</strong>
-        {!! Form::text('last_name', null, array('placeholder' => 'last_name','class' => 'form-control')) !!}
+            <strong>Nombres:</strong>
+            {!! Form::text('name', null, array('placeholder' => 'Ingrese Nombres','class' => 'form-control')) !!}
         </div>
-        </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
+    </div>
+    <div class="col-xs-12 col-sm-5">
         <div class="form-group">
-        <strong>Compañia</strong>
-        {!! Form::select('company_id', $company, null, ['class' => 'form-control']) !!}
+            <strong>Apellidos:</strong>
+            {!! Form::text('last_name', null, array('placeholder' => 'Ingrese apellidos','class' => 'form-control')) !!}
         </div>
-        </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-        <strong>Unidad</strong>
-        {!! Form::select('unity_id', $unity, null, ['class' => 'form-control']) !!}
-        </div>
-        </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-        <strong>Documento</strong>
-        {!! Form::text('document', null, array('placeholder' => 'document','class' => 'form-control')) !!}
-        </div>
-        </div>
+    </div>
+</div>
 
-    <div class="col-xs-12 col-sm-12 col-md-12">
+<div class="row">
+    <div class="col-xs-12 col-sm-4 col-md-4">
         <div class="form-group">
-            <strong>Email:</strong>
-            {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control')) !!}
+            <strong>Cargo/Ocupación:</strong>
+            {!! Form::text('position', null, array('placeholder' => 'Ingrese el cargo u ocupación','class' => 'form-control')) !!}
         </div>
     </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
+    <div class="col-xs-12 col-sm-4 col-md-4">
         <div class="form-group">
-            <strong>Password:</strong>
-            {!! Form::password('password', array('placeholder' => 'Password','class' => 'form-control')) !!}
+            <strong>Area</strong>
+            {!! Form::text('area', null, array('placeholder' => 'Ingrese el area','class' => 'form-control')) !!}
         </div>
     </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
+    <div class="col-xs-12 col-sm-4 col-md-4">
         <div class="form-group">
-            <strong>Confirmar Password:</strong>
-            {!! Form::password('confirm-password', array('placeholder' => 'Confirm Password','class' => 'form-control')) !!}
+            <strong>Gerencia:</strong>
+            {!! Form::text('management', null, array('placeholder' => 'Ingrese la gerencia','class' => 'form-control')) !!}
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-xs-12 col-sm-6 col-md-6">
+        <div class="form-group">
+            <strong>Empresa:</strong>
+            {!! Form::select('company_id', $company, null, ['class' => 'form-control']) !!}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-6">
+        <div class="form-group">
+            <strong>Unidad Minera:</strong>
+            {!! Form::select('unity_id', $unity, null, ['class' => 'form-control']) !!}
         </div>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12 text-center">
